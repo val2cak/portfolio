56e393bed23dae55092ce5d1367debc2fe5312fd
val2cak <valentina.tucak99@gmail.com>
2024-03-18 15:07:21 +0100

    fix: moving cv to contentful source

---
+++ src/constants/contentful-files.ts
@@ -0,0 +1,1 @@
+export const contentfulResumeUrl = `https://assets.ctfassets.net/${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID}/5aiwPK6lNVI2PU525QdCGt/5d8b2f61a5162e2c9eed2b379db32da2/CV_ValentinaTucak.pdf`;
